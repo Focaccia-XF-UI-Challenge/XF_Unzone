a0aba1510dc5a9cd1f476d46fc6009539011d053
宇薪 邱 <simoncat524@hotmail.com>
2019-10-27 21:38:25 +0800

    確定這次是推得上去的

--- Unzone/Unzone/Unzone/MainPage.xaml.cs
+++ Unzone/Unzone/Unzone/MainPage.xaml.cs
@@ -25,5 +25,23 @@ namespace Unzone
 #endif
             this.BindingContext = new MainViewModel();
         }
+
+        uint animationSpeed = 300;
+
+        private void TimeCellGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //淡出PopupPage
+            FadeBackground.Opacity = 0;
+            FadeBackground.IsVisible = true;
+            FadeBackground.FadeTo(1, animationSpeed);
+
+        }
+
+        private async void BackgroundTapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //取消跳出
+            await FadeBackground.FadeTo(0, animationSpeed);
+            FadeBackground.IsVisible = false;
+        }
     }
 }
